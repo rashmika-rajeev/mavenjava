8ca38893ea78266a398285d128f6c86917b51eee
rashmika-rajeev <rajeevrashmika@gmail.com>
2022-12-29 07:46:39 +0530

    first commit

---
+++ src/main/java/com/kmit1/exampleproject/App.java
@@ -0,0 +1,13 @@
+package com.kmit1.exampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
